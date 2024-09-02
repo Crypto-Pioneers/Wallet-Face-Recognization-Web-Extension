f005dd0fc5ca0b425471019e9da15338d5a251f7
friend413 <shinjirohara2@gmail.com>
2024-09-03 03:06:09 +0900

    fix issue

--- packages/extension-ui/src/Popup/ManageSeed/index.tsx
+++ packages/extension-ui/src/Popup/ManageSeed/index.tsx
@@ -1,13 +1,8 @@
 // Copyright 2019-2024 @polkadot/extension-ui authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import type { AccountWithChildren } from '@polkadot/extension-base/background/types';
+import React from 'react';
 
-import React, { useCallback, useContext, useEffect, useMemo, useState } from 'react';
-
-import getNetworkMap from '@polkadot/extension-ui/util/getNetworkMap';
-
-import { AccountContext } from '../../components/index.js';
 import { useTranslation } from '../../hooks/index.js';
 import { Header } from '../../partials/index.js';
 import { styled } from '../../styled.js';
@@ -16,66 +11,23 @@ interface Props {
   className?: string;
 }
 
-function Accounts ({ className }: Props): React.ReactElement {
+function ManageSeed ({ className }: Props): React.ReactElement {
   const { t } = useTranslation();
-  const [filter, setFilter] = useState('');
-  const [filteredAccount, setFilteredAccount] = useState<AccountWithChildren[]>([]);
-  const { hierarchy } = useContext(AccountContext);
-  const networkMap = useMemo(() => getNetworkMap(), []);
-
-  useEffect(() => {
-    setFilteredAccount(
-      filter
-        ? hierarchy.filter((account) =>
-          account.name?.toLowerCase().includes(filter) ||
-          (account.genesisHash && networkMap.get(account.genesisHash)?.toLowerCase().includes(filter)) ||
-          account.address.toLowerCase().includes(filter)
-        )
-        : hierarchy
-    );
-  }, [filter, hierarchy, networkMap]);
-
-  const _onFilter = useCallback((filter: string) => {
-    setFilter(filter.toLowerCase());
-  }, []);
 
   return (
     <>
-      {(hierarchy.length === 0)
-        ? <AddAccount />
-        : (
-          <>
-            <Header
-              onFilter={_onFilter}
-              showAdd
-              showConnectedAccounts
-              showSearch
-              showSettings
-              text={t('Accounts')}
-            />
-            <div className={className}>
-              {filteredAccount.map((json, index): React.ReactNode => (
-                <AccountsTree
-                  {...json}
-                  key={`${index}:${json.address}`}
-                />
-              ))}
-            </div>
-          </>
-        )
-      }
+      <Header
+        className={className}
+        text={t('Manage seed')}
+      />
     </>
   );
 }
 
-export default styled(Accounts)<Props>`
-  height: calc(100vh - 2px);
-  overflow-y: scroll;
-  margin-top: -25px;
-  padding-top: 25px;
-  scrollbar-width: none;
+export default styled(ManageSeed)<Props>`
+  margin-bottom: 16px;
 
-  &::-webkit-scrollbar {
-    display: none;
+  label::after {
+    right: 36px;
   }
 `;
